edfef07b8d5271cbacbb9073c8d2ac4811c38fe8
proandceo <proandceo@gmail.com>
2022-10-20 22:27:57 +1000

    added load balancer

--- src/main/java/com/example/secondservice/SecondServiceController.java
+++ src/main/java/com/example/secondservice/SecondServiceController.java
@@ -1,15 +1,24 @@
 package com.example.secondservice;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Slf4j
 @RestController
 @RequestMapping("/second-service")
 public class SecondServiceController {
+    Environment env;
+
+    public SecondServiceController(Environment env) {
+        this.env = env;
+    }
+
     @GetMapping("/welcome")
     public String welcome(){
         return "Welcome to the Second service";
@@ -19,5 +28,12 @@ public class SecondServiceController {
     public String message(@RequestHeader("second-request") String header) {
         log.info(header);
         return "Hello World in Second Service";
+    }
+
+    @GetMapping("/check")
+    public String check(HttpServletRequest request){
+        log.info(String.format("Server port=%s", request.getServerPort()));
+        return String.format("Hi, there. This is a message from Second Service on Port %s",
+                env.getProperty("local.server.port"));
     }
 }
